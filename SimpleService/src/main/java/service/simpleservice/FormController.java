7f3083759b133295e003eac4aeade56730e38a74
kushevsky01 <78921029+kushevsky01@users.noreply.github.com>
2022-06-27 18:15:01 +0300

    Add files via upload

--- SimpleService/src/main/java/service/simpleservice/FormController.java
+++ SimpleService/src/main/java/service/simpleservice/FormController.java
@@ -1,8 +1,11 @@
 package service.simpleservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.web.bind.annotation.*;
+
 import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
 
 
@@ -14,8 +17,8 @@ public class FormController {
     BookRepository bookrepo;
 
     @GetMapping("/books")
-    public List getBooks(@RequestParam(value ="author", defaultValue="no author") String author, @RequestParam (value ="title", defaultValue="no title") String title) throws IOException {
-        if(title.matches("[a-zA-z]+") &(author.matches("[a-zA-z]+"))){
+    public List getBooks(@RequestParam(value = "author", defaultValue = "no author") String author, @RequestParam(value = "title", defaultValue = "no title") String title) throws IOException {
+        if (title.matches("[a-zA-z]+") & (author.matches("[a-zA-z]+"))) {
             books.getBook(author, title);
         }
         return books.getAllBooks();
@@ -23,18 +26,36 @@ public class FormController {
 
     @GetMapping("/books/author/{author}")
     public List getBooksbyAuthor(@PathVariable("author") String author) throws IOException {
-        return bookrepo.findAllBooksByAuthor(author);
+        if (bookrepo.findAllBooksByAuthor(author).size() == 0 & author.matches("[a-zA-z]+")){
+            return Collections.singletonList("This author not found");
+        }else{
+            return bookrepo.findAllBooksByAuthor(author);
+        }
     }
+
 
     @GetMapping("/books/title/{title}")
     public List getAuthorsBook(@PathVariable("title") String title) throws IOException {
-        return bookrepo.findAuthorByTitle(title);
+        if ((bookrepo.findAuthorByTitle(title)).size() == 0 & title.matches("[a-zA-z]+")) {
+            return Collections.singletonList("Book with this title not found");
+        } else {
+            return bookrepo.findAuthorByTitle(title);
+        }
     }
 
     @GetMapping("/books/deleteById/{id}")
-    public List deleteAuthor(@PathVariable("id") int id){
-        books.delete(id);
-        return  books.getAllBooks();
+    public List deleteAuthor(@PathVariable("id") String id) {
+
+        if (id.matches("[0-9]+")){
+            try {
+                books.delete(Integer.parseInt(id));
+                return books.getAllBooks();
+            }catch (EmptyResultDataAccessException e){
+                return Collections.singletonList("id not found");
+            }
+        }else{
+            return Collections.singletonList("id must be integer");
+        }
     }
 
 }
